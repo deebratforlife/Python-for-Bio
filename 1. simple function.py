acd41592d02d11ead3c30a37426273788bf88332
Deepak Sharma <57746304+deebratforlife@users.noreply.github.com>
2022-07-27 23:09:43 -0400

    Add files via upload

---
+++ 1. simple function.py
@@ -0,0 +1,8 @@
+# Creating a function
+# In Python a function is defined using the def keyword:
+
+def my_function():
+  print("Hello from a function")
+  
+# To call a function, use the function name followed by parenthesis:
+# my_function()
