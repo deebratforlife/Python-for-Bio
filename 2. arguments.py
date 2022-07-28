5dbb9faffa1c84530e3d1125799e6882757eb922
Deepak Sharma <57746304+deebratforlife@users.noreply.github.com>
2022-07-27 23:18:11 -0400

    Add files via upload

---
+++ 2. arguments.py
@@ -0,0 +1,15 @@
+# Information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. 
+# You can add as many arguments as you want, just separate them with a comma.
+
+# The following example has a function with one argument (fname).
+
+def my_function(fname):
+  print(fname + " sequences")
+ 
+# When the function is called, we pass along a first name, 
+# which is used inside the function to print the full name:
+
+# Example
+# my_function("3' UTR")
+# Output: 3' UTR Sequences
